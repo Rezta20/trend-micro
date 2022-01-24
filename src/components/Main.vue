16e8794c05651de42ccb67ed01a8944d770f556e
Sasha <mariafu0801@gmail.com>
2022-01-25 01:15:37 +0800

    feature: layout UI login page upper

---
+++ src/components/Main.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="background">
+    <div class="card-outside-wrapper">
+      <div class="back-btn"><i class="fas fa-arrow-left"></i> Back</div>
+
+      <div class="main-content">
+        <div class="sub-title">Start from free</div>
+        <h1 class="title">Create an account</h1>
+        <div class="social-media">
+          <SocialMediaButton logo="google" text="Sign up with Google " />
+          <SocialMediaButton logo="facebook" text="Sign up with Facebook " />
+        </div>
+        <div class="description">
+          <p class="content">Or use your email for registration</p>
+          <div class="line" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SocialMediaButton from "@/components/SocialMediaButton.vue";
+
+export default {
+  name: "Main",
+  components: {
+    SocialMediaButton,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/variable.scss";
+
+* {
+  padding: 0px;
+  margin: 0px;
+}
+
+.background {
+  display: flex;
+  height: 100vh;
+  font-family: $font;
+  background-color: $background-blue;
+}
+
+.card-outside-wrapper {
+  margin: auto;
+  width: 570px;
+  padding: $base-padding;
+  border-radius: $base-radius;
+  background-color: $white;
+
+  .back-btn {
+    width: 60px;
+    margin: $base-margin;
+  }
+
+  .main-content {
+    margin: 20px 80px;
+
+    .sub-title {
+      font-weight: 400;
+      font-size: $base-font-size * 1.1;
+    }
+
+    .title {
+      font-weight: 600;
+      font-size: $base-font-size * 2;
+      margin-top: $base-margin * 0.5;
+    }
+
+    .social-media {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .description {
+      position: relative;
+      display: flex;
+      width: 100%;
+
+      .content {
+        font-weight: 500;
+      }
+      .line {
+        position: absolute;
+        width: 150px;
+        right: 0px;
+        top: 11px;
+        border-top: solid 1px black;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/SocialMediaButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <button class="wrapper">
+      <div class="icon">
+        <div v-if="logo === 'google'">
+          <i class="fab fa-google icon"></i>
+        </div>
+        <div v-if="logo === 'facebook'">
+          <i class="fab fa-facebook-f"></i>
+        </div>
+      </div>
+      <p>{{ text }}</p>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SocialMediaButton",
+  components: {},
+  props: {
+    logo: {
+      type: String,
+      default: "",
+    },
+    text: { type: String, default: "" },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/variable.scss";
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: solid 1px $button-border;
+  border-radius: $base-radius * 0.5;
+  background-color: $white;
+  box-sizing: border-box;
+  width: 195px;
+  height: 55px;
+  margin: $base-margin 0;
+
+  .icon {
+    color: $icon-color;
+    padding-right: 6px;
+    font-size: 22px;
+  }
+
+  p {
+    font-size: 13px;
+  }
+}
+</style>
